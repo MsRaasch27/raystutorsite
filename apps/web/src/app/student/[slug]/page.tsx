53f7e6e9b79090db3a061c4854208fff85f452a8
Ray Raasch <MsRaasch27@gmail.com>
2025-08-17 14:28:17 +0700

    small change

--- apps/web/src/app/student/[slug]/page.tsx
+++ apps/web/src/app/student/[slug]/page.tsx
@@ -7,7 +7,7 @@ export default async function StudentPage({
   const { slug } = await params;
   return (
     <main style={{ padding: 16 }}>
-      <h1>The Neat Student page</h1>
+      <h1>The Neato Student page</h1>
       <p>
         Slug: <code>{slug}</code>
       </p>
